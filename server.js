2eb34a9e686b41c40c6faf3c415e22509d473b5b
Julian <tipler.julian@gmail.com>
2021-05-21 13:34:55 -0700

    create server

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-let port = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(port, ()=> {
-  console.log('App is listening on 3000')
-});
+app.listen(PORT);
